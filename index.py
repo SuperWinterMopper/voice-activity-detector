47d8c0d6b9ea25fdd992c4a8b0dc6f2ce69fc85a
Keigo Healy <afkhealy@gmail.com>
2025-09-17 09:32:38 -0500

    initial setup

---
+++ index.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+import os
+import math
+import argparse
+from dataclasses import dataclass
+from typing import List, Tuple, Dict
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+import torchaudio
+from torchaudio.transforms import MelSpectrogram, AmplitudeToDB, TimeMasking, FrequencyMasking
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
