d59413982cb8e844e1227681e7c385f0f886eba4
Keigo Healy <afkhealy@gmail.com>
2025-09-23 16:20:53 -0500

    preprocessing segments

--- params.py
+++ params.py
@@ -3,3 +3,6 @@ sample_rate = 16000
 # 512 important here for speech processing, ~23ms for our data from Libriparty
 windowed_signal_length = 512
 num_mel_bands = 40
+
+# means 1 / overlap ratio of overlap for each time frame. 
+overlap = 2
